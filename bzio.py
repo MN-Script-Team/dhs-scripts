6fb16e9dd0875e070fee02bbb512f0da43b9633f
Veronica Cary <5701751+theVKC@users.noreply.github.com>
2018-01-26 16:51:07 -0600

    Initial bzio.py is complete

--- bzio.py
+++ bzio.py
@@ -2,9 +2,7 @@
 
 import win32com.client
 
-
 bz = win32com.client.Dispatch("BZWhll.WhllObj")
-
 
 
 def Connect(screen_to_connect):
@@ -12,46 +10,56 @@ def Connect(screen_to_connect):
     bz.connect(screen_to_connect)
     # TODO: error if not connected?
 
+
 def Focus():
     """Bring the BlueZone Display session window into the foreground."""
-	bz.Focus
+    bz.Focus()
 
 
-def GetCursor(RowVal, ColumnVal):
-    """Retrieve the host screen cursor position."""
-	bz.GetCursor(RowVal, ColumnVal)
+def CursorCol():
+    """Return current cursor column of the connected BZ screen."""
+    return bz.GetCursor(0, 0)[2]
+
+
+def CursorRow():
+    """Return current cursor row of the connected BZ screen."""
+    return bz.GetCursor(0, 0)[1]
 
 
 def MsgBox(message_to_deliver):
-    """Display a pop-up box from within the BlueZone window."""
+    """Display a simple pop-up box from within the BlueZone window."""
     bz.MsgBox(message_to_deliver)
 
 
-def ReadScreen(BufferStr, LengthVal, RowVal, ColumnVal):
+def ReadScreen(LengthVal, RowVal, ColumnVal):
     """Retrieve data from the host screen."""
-	bz.ReadScreen(BufferStr, LengthVal, RowVal, ColumnVal)
+    ret_details = bz.ReadScreen("", LengthVal, RowVal, ColumnVal)
+    # ret_details is a list with two items, 0 (any error code, 0 for success), and 1 (the text it read from BZ)
+    if ret_details[0] != 0:
+        raise ValueError("Either the row or column variable are too high.")
+    return ret_details[1]
 
 
 def Search(SearchStr, RowVal, ColumnVal):
-    """Searche the host screen for some specified text."""
-	bz.Search(SearchStr, RowVal, ColumnVal)
+    """Search the host screen for some specified text."""
+    return bz.Search(SearchStr, 1, 1)
 
 
 def SendKey(KeyStr):
     """Send a sequence of keys to the display session."""
-	bz.SendKey(KeyStr)
+    bz.SendKey(KeyStr)
 
 
 def SetCursor(RowVal, ColumnVal):
     """Set the host screen cursor position."""
-	bz.SetCursor(RowVal, ColumnVal)
+    bz.SetCursor(RowVal, ColumnVal)
 
 
 def WaitReady(TimeoutVal, ExtraWaitVal):
     """Suspend script execution until the host screen is ready for keyboard input."""
-	bz.WaitReady(TimeoutVal, ExtraWaitVal)
+    bz.WaitReady(TimeoutVal, ExtraWaitVal)
 
 
 def WriteScreen(WriteStr, RowVal, ColumnVal):
     """Paste the specified text into the host screen."""
-	bz.WriteScreen(WriteStr, RowVal, ColumnVal)
+    bz.WriteScreen(WriteStr, RowVal, ColumnVal)
--- testing.py
+++ testing.py
@@ -1,17 +1,24 @@
 """Test."""
 
-
+# Imports the bzio module.
 import bzio
 
-
+# Connects to BlueZone.
 bzio.Connect("")
 
-# bz.connect("")
+# Focuses the BlueZone window.
+bzio.Focus()
 
-bzio.MsgBox("Yo")
+# Reads a few characters into a variable, read_var.
+read_var = bzio.ReadScreen(4, 3, 25)
+print(read_var)
 
-x = ""
+# Now it writes those characters into a BlueZone screen.
+bzio.WriteScreen(read_var, 12, 61)
 
-bzio.ReadScreen(x, 4, 4, 34)
+# Now it prints the current CursorRow and CursorCol.
+print(bzio.CursorRow())
+print(bzio.CursorCol())
 
-print(x)
+# Lastly, we'll make a MsgBox!
+bzio.MsgBox("Thanks for running me in Python!")
